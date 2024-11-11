07ea25aa85e44af9d2a8d7b49cb62d5b0df229b5
muunseo <mkogo90@naver.com>
2024-11-12 01:32:16 +0900

    11/11

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.flo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.flo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //코틀린에서는 :이 상속을 의미함(자바에서의 extends)
+    //코틀린에서는 다른 액티비티를 상속받을 때 ()를 꼭 추가해야함
 
     lateinit var binding: ActivityMainBinding
 
@@ -12,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mainPlayerCl.setOnClickListener{
+            startActivity(Intent(this, SongActivity::class.java))
+            //Intent는 하나의 액티비티에서 사용하는 하나의 택배상자라고 생각하자.
+        }
 
         initBottomNavigation()
 
--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -1,4 +1,19 @@
 package com.example.flo
 
-class SongActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flo.databinding.ActivitySongBinding
+
+class SongActivity:AppCompatActivity () {
+    lateinit var viewBinding : ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewBinding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+
+
 }
