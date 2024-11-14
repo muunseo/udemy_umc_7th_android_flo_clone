7808a4a87cf95dafdc35ee08cca66cd273f54d55
muunseo <mkogo90@naver.com>
2024-11-14 13:26:00 +0900

    11/14 fragment실습

---
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -0,0 +1,27 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentAlbumBinding
+
+class AlbumFragment : Fragment() {
+    private lateinit var binding : FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
+        binding.btnback.setOnClickListener{
+
+        }
+
+        return binding.root
+    }
+
+}
